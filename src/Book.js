1d9cf996b7c10bc930706fa5be7dece970458d7e
Fábio Nascimento <fbn4sc@gmail.com>
2018-03-08 23:52:47 +0000

    Fetches data from the API and renders book data dynamically

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Book = () => {
+const Book = props => {
   return (
     <div className="book">
       <div className="book-top">
@@ -9,8 +9,7 @@ const Book = () => {
           style={{
             width: 128,
             height: 193,
-            backgroundImage:
-              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+            backgroundImage: `url(${props.book.imageLinks.thumbnail})`
           }}
         />
         <div className="book-shelf-changer">
@@ -25,8 +24,10 @@ const Book = () => {
           </select>
         </div>
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">
+        {props.book.authors.map((a, i) => <div key={i}>{a}</div>)}
+      </div>
     </div>
   );
 };
