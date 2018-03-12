11144b6f8cc50f9f7d3feddb240f25ca032f454a
Fábio Nascimento <fbn4sc@gmail.com>
2018-03-12 23:55:28 +0000

    Handles invalid queries

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -11,9 +11,11 @@ class SearchPage extends Component {
 
     if (searchTerm)
       search(searchTerm).then(result => {
-        this.setState({ books: result.error ? [] : result });
+        if (result.error)
+          this.setState({ books: [], error: "No books match your query." });
+        else this.setState({ books: result });
       });
-    else this.setState({ books: [] });
+    else this.setState({ books: [], error: "" });
   };
 
   render() {
@@ -24,14 +26,6 @@ class SearchPage extends Component {
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            {/*
-        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-        You can find these search terms here:
-        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-        you don't find a specific author or title. Every search is limited by search terms.
-      */}
             <input
               type="text"
               placeholder="Search by title or author"
@@ -47,6 +41,15 @@ class SearchPage extends Component {
               </li>
             ))}
           </ol>
+          {this.state.error && (
+            <div
+              style={{
+                textAlign: "center"
+              }}
+            >
+              {this.state.error}
+            </div>
+          )}
         </div>
       </div>
     );
